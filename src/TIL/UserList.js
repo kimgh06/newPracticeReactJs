5d28c20f1d52fc3c86fdc7b9aeb430d1f9fe4827
kkh061101 <kkh061101@naver.com>
2022-10-17 19:46:59 +0900

    Update UserList.js

--- src/TIL/UserList.js
+++ src/TIL/UserList.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
+  useEffect(()=>{
+    console.log("user 값 설정.");   
+    console.log(user);
+    return()=>{
+        console.log("user 값 변경 전");
+        console.log(user);
+    }
+  })
   return (
     <div>
       <b style={{cursor:'pointer', color: user.active ? 'green' : 'black'}} onClick={()=>onToggle(user.id)} >{user.username}</b> &nbsp; <span>({user.email})</span>
