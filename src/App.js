31b9ecb154fa13b8bd3c1ad806504885dfe0aa21
kkh061101 <kkh061101@naver.com>
2022-10-17 17:21:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import React, {useRef} from 'react';
 // import { render } from "react-dom"; 
 // import InputSample from './TIL/InputSample';
 import UserList from "./TIL/UserList";
+import CreateUser from "./TIL/CreateUser";
+import { useState } from "react";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email} = inputs;
+  const onChange = e =>{
+    const { name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+  }
+  const [users,setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -22,13 +36,31 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
   const nextId = useRef(4);
   const onCreate = () =>{
+    const user = {
+      id:nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+    setInputs({
+      username:'',
+      email:''
+    })
     nextId.current += 1;
   }
   return (
+  <>
+    <CreateUser
+      username={username}
+      email={email}
+      onChange={onChange}
+      onCreate={onCreate}
+    />
     <UserList users={users}/>
+  </>
   );
 }
     
