4defeb6edf0cb639a73f70e0061f5a31742d671a
kkh061101 <kkh061101@naver.com>
2022-10-17 11:29:50 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+/* eslint-disable */
+
 import "./App.css";
-import React, {Component} from 'react';// eslint-disable-line no-unused-vars
+import React, {Component} from 'react';
 import { render } from "react-dom";
 
 class App extends Component{
   render(){
     return(
-      <div>hello</div>
+      <ul>
+        <li>
+          hello
+        </li>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </ul>
     );
   }
 }
