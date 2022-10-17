e442017b0c89ec2a97b67acf9bae04a7be6cbf8b
kkh061101 <kkh061101@naver.com>
2022-10-17 16:58:04 +0900

    UserList -> App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 /* eslint-disable */
 import "./App.css";
-import React, {Component} from 'react';
-import { render } from "react-dom"; 
-import InputSample from './TIL/InputSample';
+import React, {useRef} from 'react';
+// import { render } from "react-dom"; 
+// import InputSample from './TIL/InputSample';
 import UserList from "./TIL/UserList";
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+  const nextId = useRef(4);
+  const onCreate = () =>{
+    nextId.current += 1;
+  }
   return (
-    <UserList/>
+    <UserList users={users}/>
   );
 }
     
--- src/TIL/UserList.js
+++ src/TIL/UserList.js
@@ -8,25 +8,7 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ];
-
+function UserList({users}) {
   return (
     <div>
         {users.map(user =>(
