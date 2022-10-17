61a1b9e59e12de7846b11e6ab748c296c57bdf70
kkh061101 <kkh061101@naver.com>
2022-10-17 16:15:01 +0900

    추가

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import React, {Component} from 'react';
 import { render } from "react-dom"; 
 import InputSample from './TIL/InputSample';
+import UserList from "./TIL/UserList";
 
 function App() {
   return (
-    <InputSample />
+    <UserList/>
   );
 }
     
