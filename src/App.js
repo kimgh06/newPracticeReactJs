2c1b58465cb8c02db7d85061ec2825a5c810d490
kkh061101 <kkh061101@naver.com>
2022-10-17 10:42:04 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import "./App.css";
 import React, {Component} from 'react';// eslint-disable-line no-unused-vars
 import { render } from "react-dom";
 
-function hello(){
-  return "a";
-}
-
-function App(props) {
-  return (
-    <span>{hello()}</span>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div>hello</div>
+    );
+  }
 }
     
 export default App;
