db66de93e29581541bb9604f7b63f8fbf5b93637
kkh061101 <kkh061101@naver.com>
2022-10-18 23:08:08 +0900

    Update Counter.js

--- src/TIL/Counter.js
+++ src/TIL/Counter.js
@@ -3,6 +3,7 @@ import React, { useReducer, Component } from 'react';
 class Counter extends Component{
     state = {
         number:0,
+        fixed:1,
     }
     handleIncrease = () => {
         this.setState({
@@ -20,6 +21,7 @@ class Counter extends Component{
                 <h1>{this.state.number}</h1>
                 <button onClick={this.handleIncrease}>+1</button>
                 <button onClick={this.handleDecrease}>-1</button>
+                <p>{this.state.fixed}</p>
             </div>
         );
     }
