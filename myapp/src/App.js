738415649fd09308c56732cdec00a8ab36ce81cf
kkh061101 <kkh061101@naver.com>
2022-10-19 20:35:48 +0900

    add Dummies

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,18 +5,9 @@ import './App.css';
 
 
 function App() {
-  const [id, setid] = useState([]);
-  useState(()=>{
-    const pokemon = {
-
-    }
-    fetch("https://pokeapi.co/api/v2/ability/"+{id}+"/")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-    }, []);
+  
   return (
     <div className="App">
-      aslkjf
     </div>
   );
 }
