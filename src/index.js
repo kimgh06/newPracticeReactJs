9744ff7e6966728e599248af0bb37a89f32c31ae
kkh061101 <kkh061101@naver.com>
2022-10-19 13:59:37 +0900

    user.js cnrrk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import Counter from './TIL/Counter';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Counter/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
