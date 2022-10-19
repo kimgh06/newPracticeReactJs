b1fd2b78c5729d370d9f8188913c3120697b7f56
kkh061101 <kkh061101@naver.com>
2022-10-19 17:44:24 +0900

    초기 세팅

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './Components/Button'
+
+function App() {
+  return (
+    <div className="App">
+    <div className="buttons">
+      <Button>BUTTON</Button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
