73dbf96cb04d985eb5ed8c63dcb8092b0d609f2d
kkh061101 <kkh061101@naver.com>
2022-10-19 14:29:24 +0900

    폴더명 변경

--- src/App.js
+++ src/App.js
@@ -1,76 +1,14 @@
 /* eslint-disable */
 import "./App.css";
-import produce from "immer";
 import React, {useRef , useState, useMemo, useCallback, useReducer} from 'react';
-import UserList from './TIL/UserList';
-import CreateUser from './TIL/CreateUser';
-import useInputs from './TIL/Hooks/useInputs';
-
-function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는중...');
-  return users.filter(user => user.active).length;
-}
-
-const initialState = {
-  users: [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-      active: true
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-      active: false
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-      active: false
-    }
-  ]
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'CREATE_USER':
-        return produce(state, draft =>{
-          draft.users.push(action.user);
-        });
-    case 'TOGGLE_USER':
-      return produce(state, draft =>{
-        const user = draft.users.find(user => user.id === action.id);
-        user.active = !user.active;
-      });
-    case 'REMOVE_USER':
-      return produce(state, draft=>{
-        const index = draft.users.findIndex(user => user.id === action.id);
-        draft.users.splice(index, 1);
-      })
-    default:
-      return state;
-  }
-}
-
-export const UserDispatch = React.createContext(null);
-
+import User from "./TIL1/User";
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const { users } = state;
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
+  const user = {
+    id: 1,
+    username: 'velopert'
+  };
   return (
-    <>
-      <UserDispatch.Provider value={dispatch}>
-        <CreateUser/>
-        <UserList users={users}/>
-        <div>활성사용자 수 : {count}</div>
-      </UserDispatch.Provider>
-    </>
+    <User user={user}/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Counter from './TIL/Counter';
 
 ReactDOM.render(
   <React.StrictMode>
