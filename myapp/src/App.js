6815ff228e3b7ab9afa937d7c78907f123eab356
kkh061101 <kkh061101@naver.com>
2022-10-19 20:20:35 +0900

    who that pokemon?

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import Button from './Components/Button'
+
+
 
 function App() {
+  const [id, setid] = useState([]);
+  useState(()=>{
+    const pokemon = {
+
+    }
+    fetch("https://pokeapi.co/api/v2/ability/"+{id}+"/")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+    }, []);
   return (
     <div className="App">
-    <div className="buttons">
-      <Button>BUTTON</Button>
-    </div>
+      aslkjf
     </div>
   );
 }
