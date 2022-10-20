3d5b6130a080332cdde1414be52c2f0d962c4bc4
kkh061101 <kkh061101@naver.com>
2022-10-20 20:12:59 +0900

    add span

--- app/src/Components/Button.js
+++ app/src/Components/Button.js
@@ -2,10 +2,9 @@ import React from 'react';
 import './Button.scss';
 
 
-function Button({ children, size }) {
-  
+function Button({ children, bcolor, size }) {
   return (
-    <button className="Button">{children}</button>
+    <button className="Button"><span>{children}</span></button>
   );
 }
 
