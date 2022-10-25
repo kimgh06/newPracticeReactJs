798c450dea5219ffe9375dd20c4b74751efc4586
kkh061101 <kkh061101@naver.com>
2022-10-25 18:19:37 +0900

    뻘짓1

--- cssmodule/src/components/checkBox.js
+++ cssmodule/src/components/checkBox.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
 
 function CheckBox({ children, checked, ...rest }) {
   return (
     <div>
       <label>
         <input type="checkbox" checked={checked} {...rest} />
-        <div>{checked ? '체크됨' : '체크 안됨'}</div>
+        <div>{checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}</div>
       </label>
       <span>{children}</span>
     </div>
