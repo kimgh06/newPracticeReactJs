3d663d666bce08c6da50539dbbbc2b77f1e4d268
kkh061101 <kkh061101@naver.com>
2022-10-25 17:47:51 +0900

    delete unnecessary

--- cssmodule/src/App.js
+++ cssmodule/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import CheckBox from './components/checkBox';
 
 function App() {
+  const [check, setCheck] = useState(false);
+  const onChange = e => {
+    setCheck(e.target.checked);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
+      <p>
+        <b>check: </b>
+        {check ? 'true' : 'false'}
+      </p>
     </div>
   );
 }
---
+++ cssmodule/src/components/checkBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function CheckBox({ children, checked, ...rest }) {
+  return (
+    <div>
+      <label>
+        <input type="checkbox" checked={checked} {...rest} />
+        <div>{checked ? '체크됨' : '체크 안됨'}</div>
+      </label>
+      <span>{children}</span>
+    </div>
+  );
+}
+
+export default CheckBox;
