6bccae66890a5901b5f3eb9907cb62bff19ef60c
kkh061101 <kkh061101@naver.com>
2022-10-26 20:35:50 +0900

    Commit

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 import './App.css';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #e9ecef;
+  }
+`
 
 function App() {
   return (
-    <></>
+    <>
+      <GlobalStyle/>
+      <div>hello</div>
+    </>
   );
 }
 
