51ce46c6e4dc2dad3137bf8b11856426b52b8bef
kkh061101 <kkh061101@naver.com>
2022-10-27 08:28:20 +0900

    Update TodoCreate.js

--- todo/src/components/TodoCreate.js
+++ todo/src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import { MdAdd } from 'react-icons/md';
+import { useTodoDispatch, useTodoNextId } from './TodoContext';
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -77,15 +78,33 @@ const Input = styled.input`
 
 function TodoCreate() {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('');
 
+  const dispatch =useTodoDispatch();
+  const nextId = useTodoNextId();
+  
   const onToggle = () => setOpen(!open);
-
+  const onChange = e => setValue(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault(); // 새로고침 방지
+    dispatch({
+      type: 'CREATE',
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false
+      }
+    });
+    setValue('');
+    setOpen(false);
+    nextId.current += 1;
+  };
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
-            <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" />
+          <InsertForm onSubmit={onSubmit}>
+            <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" onChange={onChange} value={value}/>
           </InsertForm>
         </InsertFormPositioner>
       )}
@@ -96,4 +115,4 @@ function TodoCreate() {
   );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
