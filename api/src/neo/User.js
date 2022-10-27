2ee74193c39e91833eba82275a5eb61c3b1e7ab8
kkh061101 <kkh061101@naver.com>
2022-10-27 17:51:48 +0900

    division codes

--- api/src/neo/User.js
+++ api/src/neo/User.js
@@ -1,55 +1,15 @@
-import React, { useEffect,useReducer}from 'react';
+import React from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
 
-function reducer(state, action){
-  switch (action.type){
-    case 'LOADING':
-      return{
-        loading:true,
-        data:null,
-        error:null
-      };
-    case 'SUCCESS':
-      return{
-        loading:false,
-        data: action.data,
-        error:null
-      };
-    case 'ERROR':
-      return{
-        loading:false,
-        data:null,
-        error:action.error
-      };
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+async function getUsers(){
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  return response.data;
 }
 
 
 function User(){
-  const [state, dispatch] = useReducer(reducer,{
-    loading:false,
-    data: null,
-    error:null
-  });
-
-  const fetchUsers = async()=>{
-    dispatch({type:'LOADING'});
-    try{
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users'
-      );
-      dispatch({type:'SUCCESS', data: response.data});
-    }
-    catch(e){
-      dispatch({type:'ERROR', error: e});
-    }
-  };
-
-  useEffect(()=>{
-    fetchUsers();
-  }, []);
+  const [state, refetch] = useAsync(getUsers,[]);
 
   const {loading, data: users, error} = state;
 
@@ -66,7 +26,7 @@ function User(){
         </li>
       ))}
     </ul>
-    <button onClick={fetchUsers}>Retry</button>
+    <button onClick={refetch}>Retry</button>
   </>
   );
 }
