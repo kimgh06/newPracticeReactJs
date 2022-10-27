75214a59d2d9c9a6dfd7f41ad64bf006fa81b9ac
kkh061101 <kkh061101@naver.com>
2022-10-27 13:28:30 +0900

    Update TodoContext.js

--- todo/src/components/TodoContext.js
+++ todo/src/components/TodoContext.js
@@ -49,7 +49,7 @@ const TodoNextIdContext = createContext();
 
 export function TodoProvider({ children }) {
     const [state, dispatch] = useReducer(todoReducer, initialTodos);
-    const nextId = useRef(5);
+    const nextId = useRef(initialTodos.length + 1);
   
     return (
       <TodoStateContext.Provider value={state}>
