46eae60f27301d4aea71e19e117146661be567e1
kkh061101 <kkh061101@naver.com>
2022-10-27 17:27:37 +0900

    using useReducer

--- api/src/neo/User.js
+++ api/src/neo/User.js
@@ -1,42 +1,73 @@
-import React, {useState , useEffect}from 'react';
+import React, { useEffect,useReducer}from 'react';
 import axios from 'axios';
 
+function reducer(state, action){
+  switch (action.type){
+    case 'LOADING':
+      return{
+        loading:true,
+        data:null,
+        error:null
+      };
+    case 'SUCCESS':
+      return{
+        loading:false,
+        data: action.data,
+        error:null
+      };
+    case 'ERROR':
+      return{
+        loading:false,
+        data:null,
+        error:action.error
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+
 function User(){
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer,{
+    loading:false,
+    data: null,
+    error:null
+  });
+
+  const fetchUsers = async()=>{
+    dispatch({type:'LOADING'});
+    try{
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      dispatch({type:'SUCCESS', data: response.data});
+    }
+    catch(e){
+      dispatch({type:'ERROR', error: e});
+    }
+  };
 
   useEffect(()=>{
-    const fetchUsers = async()=>{
-      try{
-        setError(null);
-        setUsers(null);
-        setLoading(true);
-        const response = await axios.get(
-          'https://jsonplaceholder.typicode.com/users'
-        );
-        setUsers(response.data);
-      }
-      catch(e){
-        setError(e);
-      }
-      setLoading(false);
-    };
     fetchUsers();
   }, []);
+
+  const {loading, data: users, error} = state;
 
   if(loading) return <div>loading...</div>;
   if(error) return <div>Found Errors</div>;
   if(!users) return null;
   
   return (
+  <>
     <ul>
       {users.map(user =>(
-        <li key={user.id}>
+          <li key={user.id}>
           {user.username} ({user.name})
         </li>
       ))}
     </ul>
+    <button onClick={fetchUsers}>Retry</button>
+  </>
   );
 }
 
