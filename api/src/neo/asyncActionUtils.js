6cb32f196bff51809d9a528bc25e1d9609c0f665
kkh061101 <kkh061101@naver.com>
2022-10-31 08:21:33 +0900

    Update asyncActionUtils.js

--- api/src/neo/asyncActionUtils.js
+++ api/src/neo/asyncActionUtils.js
@@ -19,3 +19,35 @@ export default function createAsyncDispatcher(type, promiseFn){
   }
   return actionHandler;
 }
+
+export const initialAsyncState = {
+  loading: false,
+  data: null,
+  error:null
+};
+
+const error = error =>({
+  loading:false,
+  data:null,
+  error:error
+});
+
+export function createAsyncHandler(type, key){
+  const SUCCESS = `${type}_SUCCESS`;
+  const ERROR = `${type}_ERROR`;
+
+  function handler(state, action){
+    switch(action.type){
+      case type:
+        return{
+          ...state,
+          [key]:loadingState
+        };
+      case SUCCESS:
+        return{
+            ...state,
+            [key]:success(action.data)
+        };
+    }
+  }
+}
