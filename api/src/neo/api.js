28773d4ccd3697a6e39d8d7760b16c6957763469
kkh061101 <kkh061101@naver.com>
2022-10-31 07:39:27 +0900

    Update api.js

---
+++ api/src/neo/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export async function getUsers(){
+  const response = await axios.get(
+    'https://jsomplaceholder.typicode.com/users'
+  );
+  return response.data;
+}
+
+export async function getUser(id){
+  const response = await axios.get
+}
